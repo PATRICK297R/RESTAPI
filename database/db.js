531afb2ae25d584d70eddc87ff9b32d19d80817f
PATRICK297R <96850073+PATRICK297R@users.noreply.github.com>
2022-06-29 19:28:40 +0700

    Update db.js

--- database/db.js
+++ database/db.js
@@ -3,9 +3,9 @@ var __path = process.cwd(),
      { color } = require(__path + '/lib/color.js')
 
 // Connection URL
-var url = 'https://api.reysekha.xyz';
+var url = 'https://api.patrick297r.xyz';
 try {
-if(url == 'https://api.reysekha.xyz') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
+if(url == 'https://api.patrick297r.xyz') throw console.log(color('Cek konfigurasi database, var url belum diisi','red'));
 } catch (e) {
 	return;
 	}
